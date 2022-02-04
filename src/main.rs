f372bb7669ad1dc0b036b5dd33a6276e29b4a5bc
Roy Tanck <roy@tanck.nl>
2022-02-04 10:26:13 +0100

    Removed the hardcoded example puzzle, which wasn't used anymore.

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,8 @@ pub struct Position {
 
 fn main() {
 
-	// default puzzle
-	let mut board = Board {
-		squares: [
-			[2,3,5,0,0,0,0,7,0],
-			[0,0,8,0,0,0,0,0,0],
-			[0,0,0,0,2,3,0,4,0],
-			[8,6,4,0,0,0,0,0,0],
-			[0,0,7,0,0,6,0,8,5],
-			[0,0,0,0,7,2,0,0,0],
-			[0,5,0,0,6,7,0,1,8],
-			[0,0,1,0,0,0,0,0,0],
-			[9,0,0,1,0,0,0,2,3],
-		]
-	};
+	// create empty puzzle
+	let mut board = Board { squares: [[0; 9] ;9] };
 
 	// get command line arguments
 
